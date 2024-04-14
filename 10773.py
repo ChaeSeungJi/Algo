45856f2b654cb1ab790819c8b1d5678fe4bfcc16
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2024-04-14 23:43:19 +0900

    Add files via upload

---
+++ 10773.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+k = int(input())
+
+
+stack = []
+
+for i in range(k):
+    x = int(input())
+
+    if x == 0:
+        stack.pop()
+    else:
+        stack.append(x)
+
+print(sum(stack))
